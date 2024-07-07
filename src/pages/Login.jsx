c651330498e67fe2c57079ac9789884f67c32b18
shafeerashafu <shafeerazahur3@gmail.com>
2024-07-07 22:56:57 +0530

    test email and password added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import axios from 'axios';
 const backendUrl = import.meta.env
                 ?import.meta.env.VITE_BE_URL //localhost
@@ -47,7 +47,10 @@ const Login = () => {
 
     };
     return (
+        <>
+           
         <div className="login">
+        
             <div className=" h-screen pt-[16vh]">
                 <form className=' ease-in duration-300 w-[80%] sm:w-max shadow-sm backdrop-blur-md bg-white/80 lg:w-max mx-auto flex flex-col items-center rounded-md px-8 py-5' onSubmit={handleOnSubmit}>
                     <NavLink to="/">
@@ -58,12 +61,14 @@ const Login = () => {
                             Email
                         </label>
                         <input type="email" placeholder='Enter your email' name='email' className=' shadow-sm bg-white appearance-none border rounded w-full py-2 px-3 sm:w-[20rem] text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
+                        <h1>Test Email : thisistestemail2501@gmail.com</h1>
                     </div>
                     <div className="mb-4">
                         <label className='block mb-2 text-sm text-gray-700' htmlFor='email'>
                             Password
                         </label>
                         <input type="password" placeholder='**********' name='password' className=' shadow-sm bg-white appearance-none border rounded w-full py-2 px-3 sm:w-[20rem] text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
+                        <h1> Test password : 12345678</h1>
                     </div>
                     <button className=" bg-[#f54748] active:scale-90 transition duration-150 transform hover:shadow-xl shadow-md w-full rounded-full px-8 py-2 text-xl font-medium text-white mx-auto text-center" type='submit'>Sign In</button>
 
@@ -73,7 +78,8 @@ const Login = () => {
                     <ToastContainer />
                 </form>
             </div>
-        </div>
+         
+        </div></>
     )
 }
 
